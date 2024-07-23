6996a42471e84b063a7e58d7ecfe5828eb1e7f4e
Ashkan Heidari <ashkanheidari@Ashkans-MacBook-Air.local>
2024-07-23 01:00:13 -0700

    Make test for views and models

--- restaurant/models.py
+++ restaurant/models.py
@@ -16,6 +16,5 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=2)
     inventory = models.IntegerField()
 
-    def __str__(self) -> str:
-        return self.title
-
+    def __str__(self):
+        return f"{self.title} : {self.price}"
