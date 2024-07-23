7592a59595730f5880da2ea7fad2f9c5a555345d
Ashkan Heidari <ashkanheidari@Ashkans-MacBook-Air.local>
2024-07-22 20:52:33 -0700

    create api for booking and register router

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu,Booking
+from rest_framework import permissions
+from .serializers import MenuSerializer,BookingSerializer,UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] 
